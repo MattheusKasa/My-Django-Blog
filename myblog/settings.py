b109ce603a8363aeeae3d0aca5010174a2be5161
MattheusKasa <mattheus.kasa@hotmail.se>
2023-03-23 14:56:18 +0000

    Add STATICFILES_DIRS to settings.py for styling

--- myblog/settings.py
+++ myblog/settings.py
@@ -128,6 +128,7 @@ LOGOUT_URL = 'logout'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
